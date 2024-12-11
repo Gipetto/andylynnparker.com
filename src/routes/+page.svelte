46d68980943248ac61fd35ebd212c2d59ea20374
Shawn Parker <shawn@topfroggraphics.com>
2024-12-11 09:42:15 -0700

    Fix weird line breaks around <del>s

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,10 +14,9 @@
       <p>I am a copywriter. I geek out about persuasive hooks and frameworks, customer research, and copywriting best practices. I want your emails, your web pages, your social paid ads. Even when I’m not writing, I’m writing.</p>
 
       <p>Former English teacher, grammar toad, wanna-be cartoon voice actor, lousy photographer, not-so-Suzy homemaker, video game dweebette, motorcyclist. Made in New York; born and raised in Denver, CO; living the dream in
-        <del>the East Bay, CA</del>
-        ,
-        <del>Vancouver, WA</del>
-        , Denver, CO.
+        <del>the East Bay, CA,</del>
+        <del>Vancouver, WA,</del>
+         Denver, CO.
       </p>
     </div>
   </div>
